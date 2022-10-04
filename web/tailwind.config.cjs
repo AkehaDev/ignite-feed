6b5d3708281b9822eb8345347b14454e63507d47
Dener Araujo <akeehaa@gmail.com>
2022-10-04 16:36:12 -0300

    Add header

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,26 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.tsx',
+    './index.html',
+  ],
   theme: {
+    fontFamily: {
+      "roboto": ['roboto', 'sans-serif']
+    },
     extend: {
-      fontFamily:{
-        "roboto":['roboto', 'sans-serif']
-      },
+      colors: {
+        "ignite-white": "#FFFFFF",
+        "ignite-gray-100": "#E1E1E6",
+        "ignite-gray-200": "#C4C4CC",
+        "ignite-gray-300": "#8D8D99",
+        "ignite-gray-400": "#7C7C8A",
+        "ignite-gray-600": "#323238",
+        "ignite-gray-700": "#202024",
+        "ignite-gray-800": "#121214",
 
-      colors:{
-        "ignite-white":"#FFFFFF",
-        "ignite-gray-100":"#E1E1E6",
-        "ignite-gray-200":"#C4C4CC",
-        "ignite-gray-300":"#8D8D99",
-        "ignite-gray-400":"#7C7C8A",
-        "ignite-gray-600":"#323238",
-        "ignite-gray-700":"#202024",
-        "ignite-gray-800":"#121214",
+        "ignite-green-100": "#00B37E",
+        "ignite-green-300": "#00875F",
 
-        "ignite-green-100":"#00B37E",
-        "ignite-green-300":"#00875F",
-
-        "ignite-red-500":"#F75A68",
+        "ignite-red-500": "#F75A68",
       }
     },
   },

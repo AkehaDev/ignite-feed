262be0605fdbb982d3b5853100f588afc9d20110
Dener Araujo <akeehaa@gmail.com>
2022-10-04 15:37:50 -0300

    Dependencies + base of the project

---
+++ web/tailwind.config.cjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily:{
+        "roboto":['roboto', 'sans-serif']
+      },
+
+      colors:{
+        "ignite-white":"#FFFFFF",
+        "ignite-gray-100":"#E1E1E6",
+        "ignite-gray-200":"#C4C4CC",
+        "ignite-gray-300":"#8D8D99",
+        "ignite-gray-400":"#7C7C8A",
+        "ignite-gray-600":"#323238",
+        "ignite-gray-700":"#202024",
+        "ignite-gray-800":"#121214",
+
+        "ignite-green-100":"#00B37E",
+        "ignite-green-300":"#00875F",
+
+        "ignite-red-500":"#F75A68",
+      }
+    },
+  },
+  plugins: [],
+}

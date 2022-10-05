cf07d8b169f5b2b1206215518db63329321ec5ac
Dener Araujo <akeehaa@gmail.com>
2022-10-05 11:43:42 -0300

    Add: components Avatar and Sidebar. Update: Global.css

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,24 @@
 import './styles/global.css'
 
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+
 
 function App() {
-  return (
-    <Header />
-  )
-}
+    return (
+      <div>
+        <Header />
+        <div className='grid grid-cols-[16rem_1fr] max-w-[70rem] mx-auto my-8 gap-8 items-start'>
+          <aside>
+            <Sidebar />
+          </aside>
+          <main>
+            <h1>main</h1>
+          </main>
+        </div>
+      </div>
+
+    )
+  }
 
 export default App

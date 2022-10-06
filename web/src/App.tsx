35f4d69cfd09cdd4bbbacb966a7f777b6499418c
Dener Araujo <akeehaa@gmail.com>
2022-10-06 16:43:03 -0300

    Added Post component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,23 +3,30 @@ import './styles/global.css'
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
 import { Post } from './components/Post'
- 
+
 
 function App() {
-    return (
-      <div>
-        <Header />
-        <div className='grid grid-cols-[16rem_1fr] max-w-[70rem] mx-auto mt-8 gap-8 items-start'>
-          <aside>
-            <Sidebar />
-          </aside>
-          <main>
-            <Post/>
-          </main>
-        </div>
+  return (
+    <div>
+      <Header />
+      <div className='grid grid-cols-[16rem_1fr] max-w-[70rem] mx-auto mt-8 gap-8 items-start'>
+        <aside>
+          <Sidebar />
+        </aside>
+        <main>
+          <Post
+            author="Akeha"
+            role="Dev Front-End"
+          />
+          <Post
+            author="Akeha"
+            role="Dev Front-End"
+          />
+        </main>
       </div>
+    </div>
 
-    )
-  }
+  )
+}
 
 export default App

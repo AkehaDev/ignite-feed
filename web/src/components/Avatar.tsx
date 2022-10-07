d51dfbe12984075c827838490bb8d90b183ac6bd
Dener Araujo <akeehaa@gmail.com>
2022-10-07 16:51:07 -0300

    Update Avatar

--- web/src/components/Avatar.tsx
+++ web/src/components/Avatar.tsx
@@ -1,7 +1,7 @@
 export function Avatar({ src }) {
   return (
     <div>
-      <img className="w-[calc(3.07rem_+_12px)] rounded-lg border-4 border-ignite-gray-700 outline outline-ignite-green-100"
+      <img className="w-[calc(3.07rem_+_12px)] h-[calc(3.07rem_+_12px)] rounded-lg border-4 border-ignite-gray-700 outline outline-ignite-green-100"
         src={src}
         alt='user avatar' />
     </div>
@@ -11,7 +11,7 @@ export function Avatar({ src }) {
 
 export function CommentAvatar({ src }) {
   return (
-    <img className="w-[calc(3.07rem_+_12px)] rounded-lg"
+    <img className="w-[calc(3.07rem_+_12px)] h-[calc(3.07rem_+_12px)] rounded-lg"
     src={src}
     alt='user avatar' />
 )
